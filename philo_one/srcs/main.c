7e28c5d0357746894f6233eeedffb6f9e16d5b1a
esta-rossa <rajiabdou9@gmail.com>
2021-01-04 23:00:35 +0100

    added logger and two forks in hand of each philosopher

--- philo_one/srcs/main.c
+++ philo_one/srcs/main.c
@@ -29,94 +29,13 @@ int error_input(int argc, char **argv)
 	return error;
 }
 
-int		get_ms_diff(struct timeval *a, struct timeval *b)
-{
-	int		sec;
-	int		u_sec;
-	int		ms_result;
-
-	sec = b->tv_sec - a->tv_sec;
-	u_sec = b->tv_usec - a->tv_usec;
-	sec *= 1e6;
-	u_sec = sec + u_sec;
-	ms_result = u_sec/1000;
-	return ms_result;
-}
-
-int		must_die(struct timeval *last_meal)
-{
-	struct	timeval	curr_time;
-	int				ms_diff;
-
-	gettimeofday(&curr_time, NULL);
-	ms_diff = get_ms_diff(last_meal, &curr_time);
-	if (ms_diff >= g_data[TDIE])
-		return 1;
-	return 0;
-}
-
-void	get_fork_indexs(int id,int fork_index[2])
-{
-	id--;
-	fork_index[0] = (id == 0) ? g_data[NPHILO] - 1 : id - 1;
-	fork_index[1] = (id == g_data[NPHILO] - 1) ? 0 : id + 1;
-}
-
-int		fork_available(int id)
-{
-	int		fork_index[2];
-
-	get_fork_indexs(id, fork_index);
-	printf("<%d %d>", fork_index[0], fork_index[1]);
-	if (fork_index[0] != fork_index[1] &&
-	g_forks[fork_index[0]] && g_forks[fork_index[1]])
-	{
-		g_forks[fork_index[0]] = 0;
-		g_forks[fork_index[1]] = 0;
-		return 1;
-	}
-	return 0;
-}
-
-void	get_food(int id, struct timeval *last_meal)
-{
-	int		fork_index[2];
-
-	gettimeofday(last_meal, NULL);
-	usleep(g_data[TEAT]);
-	get_fork_indexs(id, fork_index);
-	g_forks[fork_index[0]] = 1;
-	g_forks[fork_index[1]] = 1;
-}
-
-int		go_sleep(struct timeval lastmeal)
-{
-	int		sleep_amount;
-
-	sleep_amount = g_data[TSLEEP];
-	usleep(sleep_amount);
-	return 1;
-}
-
-int		all_alive()
-{
-	int		index;
-
-	index = 0;
-	while (index < g_data[NPHILO])
-	{
-		if (g_liveness[index] == 0)
-			return (0);
-		index++;
-	}
-	return (1);
-}
 
 void	*Philosophers(void *idptr)
 {
 	struct timeval	last_meal;
 	int				alive;
 	int				id;
+	int				hands[2];
 
 	id = (long)idptr;
 	gettimeofday(&last_meal, NULL);
@@ -124,18 +43,37 @@ void	*Philosophers(void *idptr)
 	{
 		if (must_die(&last_meal))
 		{
-			g_liveness[id - 1] = 0;
-			printf("%d dead\n", id);
+			g_liveness[id] = 0;
+			logger(id, DIED);
 			break ;
 		}
-		else if (fork_available(id))
+		fork_available(id, hands);
+		if (hands[0] && hands[1])
 		{
-			write(1, "v", 1);
+			hands[0] = 0;
+			hands[1] = 0;
 			get_food(id, &last_meal);
-			go_sleep(last_meal);
+			go_sleep(id, last_meal);
 		}
 	}
 	return (NULL);
+}
+
+void	logger(int id, int type)
+{
+	int				ms;
+	struct	timeval	now;
+
+	ms = get_ms_diff(&g_save, &now);
+	// printf("%d\n", ms);
+	ft_putnbr(ms);
+	write(1, " ", 1);
+	ft_putnbr(id);
+	type == FORK ? write(1, "has taken a fork", 16) : 1;
+	type == EATING ? write(1, "is eating", 9) : 1;
+	type == SLEEP ? write(1, " is sleeping", 12) : 1;
+	type == THINK ? write(1, "is thinking", 11) : 1;
+	type == DIED ? write(1, "died", 4) : 1;
 }
 
 int main(int argc, char **argv)
@@ -145,7 +83,7 @@ int main(int argc, char **argv)
 	// 	return 1;
 	g_data[NPHILO] = 2;
 	g_data[TDIE] = 1000;
-	g_data[TEAT] = 3000;
+	g_data[TEAT] = 500;
 	g_data[TSLEEP] = 1000;
 	g_data[NTPEAT] = -1;
 	// while (argc)
@@ -156,12 +94,14 @@ int main(int argc, char **argv)
 	g_forks = (int *)malloc(sizeof(int) * g_data[NPHILO]);
 	g_liveness = (int *)malloc(sizeof(int) * g_data[NPHILO]);
 	threads = (pthread_t *)malloc(sizeof(pthread_t) * g_data[NPHILO]);
-	g_forks[0] = 1;
-	g_liveness[0] = 1;
-	g_liveness[1] = 1;
-	g_forks[1] = 1;
-	pthread_create(&threads[0], NULL, Philosophers, (void *)1);
-	pthread_create(&threads[1], NULL, Philosophers, (void *)2);
-	pthread_join(threads[0], NULL);
-	pthread_join(threads[1], NULL);
+	for (size_t i = 0; i < g_data[NPHILO]; i++)
+	{
+		g_forks[i] = 1;
+		g_liveness[i] = 1;
+	}
+	gettimeofday(&g_save, NULL);
+	for (size_t i = 0; i < g_data[NPHILO]; i++)
+	{
+		pthread_create(&threads[0], NULL, Philosophers, (void *)i);
+	}
 }
