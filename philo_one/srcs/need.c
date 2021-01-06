72667e1a17486051a46c7ccc442e734e25dd1f85
esta-rossa <rajiabdou9@gmail.com>
2021-01-06 15:25:36 +0100

    added read from argv fixed code structure, added mutex before logger and forks checker

--- philo_one/srcs/need.c
+++ philo_one/srcs/need.c
@@ -32,15 +32,14 @@ void	get_fork_indexs(int id,int fork_index[2])
 	fork_index[1] = id;
 }
 
-int		fork_available(int id, int hands[2])
+void		fork_available(int id, int hands[2])
 {
 	int		fork_index[2];
 	int		*pick;
 
 	get_fork_indexs(id, fork_index);
 	pick = hands[0] ? &hands[1] : &hands[0];
-	if (fork_index[0] == fork_index[1])
-		return 0;
+	pthread_mutex_lock(&g_forks_mutex);
 	if (g_forks[fork_index[0]])
 	{
 		g_forks[fork_index[0]] = 0;
@@ -53,7 +52,7 @@ int		fork_available(int id, int hands[2])
 		*pick = 1;
 		logger(id, FORK);
 	}
-	return (*pick);
+	pthread_mutex_unlock(&g_forks_mutex);
 }
 
 void	get_food(int id, struct timeval *last_meal)
@@ -61,33 +60,44 @@ void	get_food(int id, struct timeval *last_meal)
 	int		fork_index[2];
 
 	logger(id, EATING);
-	gettimeofday(last_meal, NULL);
+	g_eat_amount[id]++;
 	usleep(g_data[TEAT] * 1000);
+	gettimeofday(last_meal, NULL);
 	get_fork_indexs(id, fork_index);
 	g_forks[fork_index[0]] = 1;
 	g_forks[fork_index[1]] = 1;
 }
 
-int		go_sleep(int id, struct timeval lastmeal)
+void	go_sleep(int id, struct timeval lastmeal)
 {
-	int		sleep_amount;
-
 	logger(id, SLEEP);
-	sleep_amount = g_data[TSLEEP];
-	usleep(sleep_amount);
-	return 1;
+	usleep(g_data[TSLEEP] * 1000);
 }
 
-int		all_alive()
+void		*all_alive(void *ptr)
 {
 	int		index;
+	int		philos;
 
-	index = 0;
-	while (index < g_data[NPHILO])
+	index = (long)ptr;
+	while (1)
 	{
+		philos = -1;
 		if (g_liveness[index] == 0)
-			return (0);
+		{
+			g_all_alive = 0;
+			break ;
+		}
+		while (g_data[NTPEAT] != -1 && ++philos < g_data[NPHILO])
+		{
+			if (g_eat_amount[philos] < g_data[NTPEAT])
+				break ;
+			if (philos == g_data[NPHILO] - 1)
+				g_all_alive = 0;
+		}
 		index++;
+		if (index == g_data[NPHILO])
+			index = 0;
 	}
-	return (1);
+	return (NULL);
 }
