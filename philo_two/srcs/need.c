dc3a7de0e1b3f5e9ea0cb375350a41a6cd9de3a1
Rraji Abdelbari <rajiabdou9@gmail.com>
2021-05-18 14:37:33 +0100

    norm need.c files

--- philo_two/srcs/need.c
+++ philo_two/srcs/need.c
@@ -1,6 +1,36 @@
 #include "philo_two.h"
 
-int		get_ms_diff(struct timeval *a, struct timeval *b)
+int	error_input(int argc, char **argv)
+{
+	int		error;
+	int		index;
+	int		jndex;
+
+	error = 0;
+	index = 1;
+	jndex = 0;
+	if (argc != 5 && argc != 6)
+		error = 1;
+	else
+	{
+		while (argv[index] && argv[index][jndex] && !error)
+		{
+			if (argv[index][jndex] < '0' || argv[index][jndex] > '9')
+				error = 1;
+			jndex++;
+			if (argv[index][jndex] == '\0')
+			{
+				index++;
+				jndex = 0;
+			}
+		}
+	}
+	if (error)
+		write(STDIN_FILENO, "Error input\n", 12);
+	return (error);
+}
+
+int	get_ms_diff(struct timeval *a, struct timeval *b)
 {
 	int		sec;
 	int		u_sec;
@@ -10,23 +40,23 @@ int		get_ms_diff(struct timeval *a, struct timeval *b)
 	u_sec = b->tv_usec - a->tv_usec;
 	sec *= 1e6;
 	u_sec = sec + u_sec;
-	ms_result = u_sec/1000;
-	return ms_result;
+	ms_result = u_sec / 1000;
+	return (ms_result);
 }
 
-int		must_die(struct timeval *last_meal)
+int	must_die(struct timeval *last_meal)
 {
-	struct	timeval	curr_time;
+	struct timeval	curr_time;
 	int				ms_diff;
 
 	gettimeofday(&curr_time, NULL);
 	ms_diff = get_ms_diff(last_meal, &curr_time);
 	if (ms_diff >= g_data[TDIE])
-		return 1;
-	return 0;
+		return (1);
+	return (0);
 }
 
-void		fork_available(int id)
+void	fork_available(int id)
 {
 	sem_wait(g_forks_sema);
 	logger(id, FORK);
@@ -52,7 +82,7 @@ void	go_sleep(int id, struct timeval lastmeal)
 	usleep(g_data[TSLEEP] * 1000);
 }
 
-void		*liveness_thread(void *ptr)
+void	*liveness_thread(void *ptr)
 {
 	t_philo		*me;
 
